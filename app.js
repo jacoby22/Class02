c0281fa6ac1b0b8d4e62673f12fb67fde3911426
Nick Jacoby <nickjacoby22@gmail.com>
2016-05-25 21:06:16 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// var userName = prompt('What\'s your name?');
+// alert('Welcome to my page, ' + userName);
+
+
+var questionOne = prompt('What state was I born in?').toLowerCase();
+// questionOne.toLowerCase();
+
+if (questionOne === 'washington') {
+  alert('Yay, you know me so well! \n Gold Star!');
+  //do something;
+}
+else {
+  alert('Sorry, you no know me... Sad trombone.');
+  //do something else;
+}
